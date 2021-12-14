fe61dc8b4548ce9035874ad3f1017f74ed6f8bb8
Rahul <drahul.official@gmail.com>
2021-12-14 11:24:19 +0530

    calculation is added

--- Diff.java
+++ Diff.java
@@ -1,5 +1,8 @@
 public class Diff{
- public static void main(String[] args) {
-     
+    public static void main(String[] args)
+
+    int x = 5;
+    int y = 6;
+
+    System.out.println("This diff is" + (x-y));
  }   
-}
